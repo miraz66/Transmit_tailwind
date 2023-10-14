1621794507396bb71882bbfe0dd18114428a27cb
miraz66 <mirajulislam0509@gmail.com>
2023-10-14 14:05:12 +0600

    Transmit_tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,116 @@
-import Image from 'next/image'
+import Center_body from "@/components/Center_body";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen">
+      <div className="flex justify-between">
+        <div className="flex flex-cols">
+          <div className="bg-slate-50 min-h-screen border-r">
+            <div className="hidden lg:sticky lg:top-0 lg:flex lg:w-16 lg:flex-none lg:items-center lg:whitespace-nowrap lg:py-12 lg:text-sm lg:leading-7 lg:[writing-mode:vertical-rl]">
+              <span className="font-mono text-slate-500">Hosted by</span>
+              <span className="mt-6 flex gap-6 font-medium text-slate-900 tracking-wide">
+                Eric Gordon
+                <span
+                  aria-hidden="true"
+                  className="text-slate-400 tracking-wide"
+                >
+                  /
+                </span>
+                Wes Mantooth
+              </span>
+            </div>
+          </div>
+          <div className="bg-slate-50 w-[26rem] h-screen overflow-y-auto">
+            <Center_body />
+          </div>
+        </div>
+
+        <div className="bg-red-200 h-screen overflow-y-auto p-10">
+          A well-organized paragraph supports or develops a single controlling
+          idea, which is expressed in a sentence called the topic sentence. A
+          topic sentence has several important functions: it substantiates or
+          supports an essay’s thesis statement; it unifies the content of a
+          paragraph and directs the order of the sentences; and it advises the
+          reader of the subject to be discussed and how the paragraph will
+          discuss it. Readers generally look to the first few sentences in a
+          paragraph to determine the subject and perspective of the paragraph.
+          That’s why it’s often best to put the topic sentence at the very
+          beginning of the paragraph. In some cases, however, it’s more
+          effective to place another sentence before the topic sentence—for
+          example, a sentence linking the current paragraph to the previous one,
+          or one providing background information. Although most paragraphs
+          should have a topic sentence, there are a few situations when a
+          paragraph might not need a topic sentence. For example, you might be
+          able to omit a topic sentence in a paragraph that narrates a series of
+          events, if a paragraph continues developing an idea that you
+          introduced (with a topic sentence) in the previous paragraph, or if
+          all the sentences and details in a paragraph clearly refer—perhaps
+          indirectly—to a main point. The vast majority of your paragraphs,
+          however, should have a topic sentence. A well-organized paragraph
+          supports or develops a single controlling idea, which is expressed in
+          a sentence called the topic sentence. A topic sentence has several
+          important functions: it substantiates or supports an essay’s thesis
+          statement; it unifies the content of a paragraph and directs the order
+          of the sentences; and it advises the reader of the subject to be
+          discussed and how the paragraph will discuss it. Readers generally
+          look to the first few sentences in a paragraph to determine the
+          subject and perspective of the paragraph. That’s why it’s often best
+          to put the topic sentence at the very beginning of the paragraph. In
+          some cases, however, it’s more effective to place another sentence
+          before the topic sentence—for example, a sentence linking the current
+          paragraph to the previous one, or one providing background
+          information. Although most paragraphs should have a topic sentence,
+          there are a few situations when a paragraph might not need a topic
+          sentence. For example, you might be able to omit a topic sentence in a
+          paragraph that narrates a series of events, if a paragraph continues
+          developing an idea that you introduced (with a topic sentence) in the
+          previous paragraph, or if all the sentences and details in a paragraph
+          clearly refer—perhaps indirectly—to a main point. The vast majority of
+          your paragraphs, however, should have a topic sentence.A
+          well-organized paragraph supports or develops a single controlling
+          idea, which is expressed in a sentence called the topic sentence. A
+          topic sentence has several important functions: it substantiates or
+          supports an essay’s thesis statement; it unifies the content of a
+          paragraph and directs the order of the sentences; and it advises the
+          reader of the subject to be discussed and how the paragraph will
+          discuss it. Readers generally look to the first few sentences in a
+          paragraph to determine the subject and perspective of the paragraph.
+          That’s why it’s often best to put the topic sentence at the very
+          beginning of the paragraph. In some cases, however, it’s more
+          effective to place another sentence before the topic sentence—for
+          example, a sentence linking the current paragraph to the previous one,
+          or one providing background information. Although most paragraphs
+          should have a topic sentence, there are a few situations when a
+          paragraph might not need a topic sentence. For example, you might be
+          able to omit a topic sentence in a paragraph that narrates a series of
+          events, if a paragraph continues developing an idea that you
+          introduced (with a topic sentence) in the previous paragraph, or if
+          all the sentences and details in a paragraph clearly refer—perhaps
+          indirectly—to a main point. The vast majority of your paragraphs,
+          however, should have a topic sentence. A well-organized paragraph
+          supports or develops a single controlling idea, which is expressed in
+          a sentence called the topic sentence. A topic sentence has several
+          important functions: it substantiates or supports an essay’s thesis
+          statement; it unifies the content of a paragraph and directs the order
+          of the sentences; and it advises the reader of the subject to be
+          discussed and how the paragraph will discuss it. Readers generally
+          look to the first few sentences in a paragraph to determine the
+          subject and perspective of the paragraph. That’s why it’s often best
+          to put the topic sentence at the very beginning of the paragraph. In
+          some cases, however, it’s more effective to place another sentence
+          before the topic sentence—for example, a sentence linking the current
+          paragraph to the previous one, or one providing background
+          information. Although most paragraphs should have a topic sentence,
+          there are a few situations when a paragraph might not need a topic
+          sentence. For example, you might be able to omit a topic sentence in a
+          paragraph that narrates a series of events, if a paragraph continues
+          developing an idea that you introduced (with a topic sentence) in the
+          previous paragraph, or if all the sentences and details in a paragraph
+          clearly refer—perhaps indirectly—to a main point. The vast majority of
+          your paragraphs, however, should have a topic sentence.
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
---
+++ src/components/Center_body/index.js
@@ -0,0 +1,2 @@
+export * from "./Center_body";
+export { default } from "./Center_body";
