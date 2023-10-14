be44e1dbd0b144eed74fa5fe137e81a79c1bf1aa
miraz66 <mirajulislam0509@gmail.com>
2023-10-14 22:40:31 +0600

    Icon

--- src/assets/Icon/svg.jsx
+++ src/assets/Icon/svg.jsx
@@ -173,11 +173,11 @@ export const Spotify = ({ className }) => (
 //   </svg>
 // );
 
-// export const PlayIcon = ({ className }) => (
-//   <svg aria-hidden="true" viewBox="0 0 10 10" className={className}>
-//     <path d="M8.25 4.567a.5.5 0 0 1 0 .866l-7.5 4.33A.5.5 0 0 1 0 9.33V.67A.5.5 0 0 1 .75.237l7.5 4.33Z"></path>
-//   </svg>
-// );
+export const PlayIcon = ({ className }) => (
+  <svg aria-hidden="true" viewBox="0 0 10 10" className={className}>
+    <path d="M8.25 4.567a.5.5 0 0 1 0 .866l-7.5 4.33A.5.5 0 0 1 0 9.33V.67A.5.5 0 0 1 .75.237l7.5 4.33Z"></path>
+  </svg>
+);
 
 export const PauseIcon = ({ className }) => (
   <svg aria-hidden="true" viewBox="0 0 10 10" className={className}>
