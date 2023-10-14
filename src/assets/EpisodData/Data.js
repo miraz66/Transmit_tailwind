03fa316a8e29a3fcdb8de186a9218b548414e4e7
miraz66 <mirajulislam0509@gmail.com>
2023-10-14 22:36:54 +0600

    data

--- src/assets/EpisodData/Data.js
+++ src/assets/EpisodData/Data.js
@@ -1,12 +1,12 @@
 export const Data = [
-  //   {
-  //     id: 1,
-  //     date: "February 24, 2022",
-  //     title: "Bill Lumbergh",
-  //     description:
-  //       "Bill LumberghHe’s going to need you to go ahead and come in on Saturday, but there’s a lot more to the story than youthink.",
-  //     audio: "",
-  //   },
+  {
+    id: 1,
+    date: "February 24, 2022",
+    title: "Bill Lumbergh",
+    description:
+      "Bill LumberghHe’s going to need you to go ahead and come in on Saturday, but there’s a lot more to the story than youthink.",
+    audio: "",
+  },
   {
     id: 2,
     date: "February 24, 2022",
