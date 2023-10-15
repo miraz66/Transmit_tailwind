a321342e88f40bfb465db4353ba498e9709bf338
miraz66 <mirajulislam0509@gmail.com>
2023-10-15 23:35:13 +0600

    number

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -152,6 +152,8 @@ export default function page({ params }) {
                               <div>dsfsdfsd</div>
                               <div>dsfsdfsd</div>
                               <div>dsfsdfsd</div>
+                              <div>dsfsdfsd</div>
+                              <div>dsfsdfsd</div>
                             </div>
                           </div>
                         </article>
