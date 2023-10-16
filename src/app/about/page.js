456d26f95ca382318d1a7560c861461d59bc748c
miraz66 <mirajulislam0509@gmail.com>
2023-10-16 23:57:11 +0600

    About

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -28,6 +28,8 @@ export default function Page() {
         <p>{count}</p>
         <p>{count}</p>
         <p>{count}</p>
+        <p>{count}</p>
+        <p>{count}</p>
 
         <button onClick={() => countIncrement()}>Increment</button>
       </div>
