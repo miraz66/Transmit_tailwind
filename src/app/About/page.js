b99a0a4e1ec1f6b30b178b025fd231d3f4e989d9
miraz66 <mirajulislam0509@gmail.com>
2023-10-16 23:49:53 +0600

    About

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -6,7 +6,12 @@ export default function Page() {
   const [count, setCount] = useState(0);
   const countRef = useRef(0);
 
-  function countIncrement(count) {}
+  function countIncrement() {
+    setCount(count + 1);
+    countRef.current++;
+
+    console.log("count: " + count);
+  }
 
   return <div>page</div>;
 }
