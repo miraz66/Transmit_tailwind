41ed4163cba6b36ae07c3c2555a6969b74f7e812
miraz66 <mirajulislam0509@gmail.com>
2023-10-16 23:56:38 +0600

    About

---
+++ src/app/about/page.js
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRef, useState } from "react";
+
+export default function Page() {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+
+  const countIncrement = () => {
+    setCount(count + 1);
+    countRef.current++;
+
+    console.log("count: " + count);
+    console.log("countRef: " + countRef.current);
+  };
+
+  return (
+    <div>
+      <div className="h-screen flex flex-col justify-center items-center">
+        <p>{count}</p>
+
+        <button onClick={() => countIncrement()}>Increment</button>
+      </div>
+    </div>
+  );
+}
