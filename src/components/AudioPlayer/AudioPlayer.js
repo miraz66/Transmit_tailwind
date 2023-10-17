eeb06178c5c1f613fdf44153e4586c9b29377b79
miraz66 <mirajulislam0509@gmail.com>
2023-10-17 23:37:00 +0600

    audio

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -283,6 +283,8 @@ function AudioPlayer({ isPlaying, setIsPlaying, currentSong }) {
       <div>audio</div>
       <div>audio</div>
       <div>audio</div>
+      <div>audio</div>
+      <div>audio</div>
     </>
   );
 }
